76744d69537bded523710667271ad6b005fb04cb
agrawalayushi23 <151565089+agrawalayushi23@users.noreply.github.com>
2023-11-21 11:13:45 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -89,7 +89,26 @@ menuIcon.onclick = () => {
 window.onscroll = () => {
         menuIcon.classList.remove("bx-x");
         navList.classList.remove("open");
-}
+};
+const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                        entry.target.classList.add("show-items");
+                } else {
+                        entry.target.classList.remove("show-items");
+                }
+                
+        });
+});
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el) => observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el) => observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el) => observer.observe(el));
+
 
 
 
